ed5a77f58513a9af95703d0616a657c345f6aef5
shamineoduor <52905732+shamineoduor@users.noreply.github.com>
2019-07-20 14:23:27 +0300

    changes title

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -29,6 +29,8 @@ export class LoginComponent implements OnInit {
       this.router.navigate(['/']);
 
       this.loginFormGroup.reset();
+    } else {
+      alert ("incorrect login credentials");
     }
   }
 }
